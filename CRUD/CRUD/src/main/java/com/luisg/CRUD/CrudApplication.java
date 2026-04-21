dcfd2bd1cbb290e9dd70e3ca6786bde05a21e5d4
LuisOliveira17 <gustavokreft@gmail.com>
2026-04-21 20:42:22 -0300

    Preparando ambiente

---
+++ CRUD/CRUD/src/main/java/com/luisg/CRUD/CrudApplication.java
@@ -0,0 +1,13 @@
+package com.luisg.CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+}
---
+++ CRUD/CRUD/src/test/java/com/luisg/CRUD/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisg.CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
