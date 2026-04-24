1674d0f4baf860f37d22872c987a0f15d972001a
LuisOliveira17 <gustavokreft@gmail.com>
2026-04-24 17:01:22 -0300

    Criado a lógica do DTO de Request

--- CRUD/CRUD/src/main/java/com/luisg/CRUD/dto/ComidaRequestDto.java
+++ CRUD/CRUD/src/main/java/com/luisg/CRUD/dto/ComidaRequestDto.java
@@ -1,4 +1,41 @@
 package com.luisg.CRUD.dto;
 
+//O request será apenas um DTO para Entrada de dados. Não terá Setters
+//Também não terá ID, pois é apenas uma classe de entrada de dados. O ID é gerado na camada de BD
+//Aqui, se trata as validações de entrada do usuário
+
+import java.time.LocalDate;
+
 public class ComidaRequestDto {
+    private final String nome;
+    private final Integer quantidade;
+    private final LocalDate dataValidade;
+
+    public ComidaRequestDto(String nome, Integer quantidade, LocalDate dataValidade) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.dataValidade = dataValidade;
+    }
+
+    public void validar(String nome, Integer quantidade, LocalDate dataValidade) {
+        if(nome == null || nome.isBlank()){
+            throw new IllegalArgumentException("O campo nome deve ser preenchido");
+        }
+
+        else if(quantidade<0){
+            throw new IllegalArgumentException("A quantidade deve ser maior que zero");
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public LocalDate getDataValidade() {
+        return dataValidade;
+    }
 }
